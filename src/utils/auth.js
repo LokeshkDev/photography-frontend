3a2cc626296ef51ba4fc5f0f4381843686b15924
lokeshk2492 <lokeshk2492@gmail.com>
2025-11-28 20:00:56 +0530

    pointing to global

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "https://your-backend-url.com/api/auth";
-
-const API_URL = "http://localhost:5000/api/";
-
+// const API_URL = "http://localhost:5000/api/";
+const API_URL = "https://photography-backend-ivir.onrender.com/api/";
 export const login = async (username, password) => {
   try {
     const res = await axios.post(
